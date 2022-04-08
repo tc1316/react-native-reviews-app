0adadc05ec3b69f51d961b8e7dd71e9bc6dc29cb
Titus Chang <titus@Tituss-MacBook-Pro.local>
2022-04-08 14:25:39 +0100

    Modern approach to font loading

--- App.js
+++ App.js
@@ -2,27 +2,16 @@ import * as Font from "expo-font";
 
 import AppLoading from "expo-app-loading";
 import Home from "./screens/Home";
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
 
-const getFonts = () =>
-  Font.loadAsync({
+export default function App() {
+  const [fontsLoaded] = Font.useFonts({
     "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
     "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
   });
 
-export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  if (fontsLoaded) {
-    return <Home />;
-  } else {
-    return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={() => setFontsLoaded(true)}
-        onError={() => console.log("error")}
-      />
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
+
+  return <Home />;
 }
