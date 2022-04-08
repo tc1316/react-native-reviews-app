3722af4f96d6365b254e9014e72c9216a283f735
Titus Chang <titus@Tituss-MacBook-Pro.local>
2022-04-08 12:12:40 +0100

    Setup screens

---
+++ screens/Home.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
